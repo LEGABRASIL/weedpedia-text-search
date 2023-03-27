1cf1722603960bc3cde1e2d4dbc19897327c2253
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2023-03-27 17:46:48 -0300

    pagination NUMBER FIXING

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -12,15 +12,15 @@ function PaginationButtons() {
   const startIndex = Number(router.query.start) || 0;
   return (
     <div className="flex max-w-lg justify-between text-yellow-500 mb-10">
-      {startIndex >=10 && (
-        <Link href={`/search?term=${router.query.term}&start=${startIndex - 10}&sort=${sort}`}>
+      {startIndex >=25 && (
+        <Link href={`/search?term=${router.query.term}&start=${startIndex - 25}&sort=${sort}`}>
           <div className='flex flex-grow flex-col items-center cursor-pointer hover:underline'>
             <ChevronLeftIcon className="h-5"/>
             <p className='text-orange-700'>Previous</p>
           </div>
         </Link>
       )}
-      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}&sort=${sort}`}>
+      <Link href={`/search?term=${router.query.term}&start=${startIndex + 25}&sort=${sort}`}>
         <div className='flex flex-grow flex-col items-center cursor-pointer hover:underline' >
           <ChevronRightIcon className="h-5"/>
           <p className='text-orange-700'>Next</p>
