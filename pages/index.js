b476f0b2b90d2697f785db9002d8d8a40db0f9a9
donzito <felipe@mobees.com.br>
2023-03-29 07:27:53 -0300

    Favicon fix, logo size update

--- pages/index.js
+++ pages/index.js
@@ -28,26 +28,46 @@ export default function Home() {
       
       <Head>
         <title>Weedpedia Text search BETA</title>
-        <link rel="icon" href="/weedpedia-icon.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       
       {/* Body */}
         <form className="flex flex-col items-center w-4/5">
-        <Image 
-          src="/logo-weedpedia-color.svg" 
-          className="logo mb-12"
-          height={80}
-          width={585}
-          />
-          <div className="flex bg-white w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-secondary-300 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
-            <SearchIcon className="h-5 mr-3 text-secondary-500"/>
-            <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
+          <div
+            className="logo-container"
+          >
+            <Image 
+              src="/logo-weedpedia-color.svg" 
+              className="logo mb-12"
+              layout='fill'
+            />
+          </div>
+          <div 
+            className="flex bg-white w-full mt-12 transition-[box-shadow] duration-500 ease-in-out hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-secondary-300 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
+          >
+            <SearchIcon 
+              className="h-5 mr-3 text-secondary-500"
+            />
+            <input 
+              ref={searchInputRef} 
+              type="text" 
+              placeholder="Find cannabis news, legislation, products and more"
+              className="flex-grow focus:outline-none" 
+            />
           </div>
           
           {/* Buttons */}
           <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-            <button onClick={(event)=>search(event)} className="btn"> URL beta finder</button>
+            <button 
+              onClick={(event)=>search(event)} 
+              className="btn"
+            > 
+              URL beta finder
+            </button>
           </div>
         </form>
     </div>
