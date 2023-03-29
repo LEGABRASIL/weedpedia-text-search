dd31ef0457c83b56034c1eab154c2c5d71751d11
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2023-03-28 23:29:59 -0300

    hiding menu button

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -6,7 +6,7 @@ function HeaderOptions(props) {
     <div className="flex w-full text-orange-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b">
       <div className="flex space-x-6">
         <HeaderOption Icon={SearchIcon} title="Busca pela URL" selected method={props.regularSearch}/>
-        <HeaderOption Icon={FolderOpenIcon} title="Busca por Metadados" method={props.searchByDate}/>
+        {/*<HeaderOption Icon={FolderOpenIcon} title="Busca por Metadados" method={props.searchByDate}/>*/}
       </div>
     </div>
   )
